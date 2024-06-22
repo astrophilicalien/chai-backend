26c9682fd4d5c6e4233d8916ad8365160d6f2a05
haridev <astrophilicalien@gmail.com>
2024-06-23 00:14:20 +0530

    Database connect

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+connectDB();
+
+
+
+
+
+
+/*
+;(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (error)=>{
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening successfully on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ", error);
+        throw err
+    }
+})()
+    
+*/
