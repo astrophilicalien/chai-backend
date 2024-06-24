daeb65e06daf8e504b98719d4937a5fe6abb1b8f
haridev <astrophilicalien@gmail.com>
2024-06-24 20:47:28 +0530

    add api utils files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 3000, () =>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log(`MONGODB connection failed !!!`, err)
+})
+
+
+
+
+
+
 
 
 
