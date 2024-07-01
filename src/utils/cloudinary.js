ccda639d86458c60c2194835744d03f9b12495f6
astrophilicalien <astrophilicalien@gmail.com>
2024-07-01 21:19:13 +0530

    add multer middleware

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,3 +7,25 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
 });
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        
+        if(!localFilePath) return null
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfully
+        console.log("file is uploaded on cloudinary", response.url);
+
+        return response;
+
+    } catch (error) {
+        
+        fs.unlinkSync(localFilePath) // removes the locally saved temporary files as the upload failed
+        return null;
+
+    }
+}
