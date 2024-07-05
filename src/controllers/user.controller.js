3d62aeda3a8413842608f3cd1d55aecbd77afa74
astrophilicalien <astrophilicalien@gmail.com>
2024-07-05 22:11:11 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,20 +5,21 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+    // res.status(200).json({
+    //     message: "ok"
+    // }) 
 
 
     const {fullName, email, username, password} = req.body;
 
-    console.log("email: ", email);
+    // console.log("email: ", email);
+    // console.log(req.body);
 
     if([fullName, email, username, password].some((field) => field?.trim() === "")){
         throw new ApiError(400, "All fields are compulsary ");
     }
 
-    const existedUser = User.findOne(
+    const existedUser = await User.findOne(
         {
             $or: [{fullName}, {email}]
         }
@@ -28,9 +29,16 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(409, "User with username or email already exists")
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // console.log(req.files);
 
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+  
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required");
     }
