24f13cd9b62290877a9d8b99c919d495365c28fd
astrophilicalien <astrophilicalien@gmail.com>
2024-07-10 12:53:14 +0530

    bug fixes in user controller and auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -100,7 +100,7 @@ const loginUser = asyncHandler( async (req, res) => {
 
     const {email, username, password} = req.body; //1
 
-    if(!username || !email){
+    if(!username && !email){
         throw new ApiError(400, "username or email is required") //2
     }
 
